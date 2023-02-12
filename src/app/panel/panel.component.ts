546e38aa635e6c8d63e9278f0d26af9cdf75a0a6
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-12 13:40:21 +0100

    create a panel component and pass a dinamic variables

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-panel',
+  selector: 'panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
