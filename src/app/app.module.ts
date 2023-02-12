6b68605c35a832a93d166a59fde03ec28844cda3
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-12 13:41:03 +0100

    generate a courses component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { PanelComponent } from './panel/panel.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FavoriteComponent,
-    PanelComponent
+    PanelComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
