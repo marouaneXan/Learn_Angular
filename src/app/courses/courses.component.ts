b9f70e9321b1172bb40ceac344a2da35795e382d
marouaneXan <marouanezahaoui835@gmail.com>
2023-02-12 14:16:30 +0100

    add *ngIf directive

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-courses',
+  selector: 'courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
-
+  courses=[]
 }
